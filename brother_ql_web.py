0d114ff2cd68e982917a71bc2f58b75fea95295a
Christopher Vergaray <veryoutspoken6@gmail.com>
2024-07-03 15:11:25 -0400

    Improved the image elements position and size options: - Allowing position to be specified as either 1 coordinate (top left corner)  or 2 coordinates (top left and bottom right corners). This maintains backwards compatibility with previous lbl formats. - If position is provided as one coordinate, then either or both of Width and Height can be specified. -- If neither is specified, no scaling is performed. -- If only one is provided, the image will be scaled based on the specified dimension.  The scaling is proportional if maintainAR is TRUE. -- If both are provided, the image will be scaled to the height and width. If maintainAR is TRUE, the image will be scaled proportionally to fit within the specified width and height while maintaining the aspect ratio.

--- brother_ql_web.py
+++ brother_ql_web.py
@@ -378,12 +378,40 @@ def element_image_url(element, im, margins, dimensions, **kwargs):
 
 def element_image_base(image_to_add, element, im, margins, dimensions, **kwargs):
     try:
-        position = element.get('position')
+        position = element.get('position', None)
 
-        image_to_add = image_to_add.resize(
-            (position[2] - position[0],
-             position[3] - position[1])
-        )
+        width = element.get('width', None)
+        height = element.get('height', None)
+        maintainAR = element.get('maintainAR', True)
+
+        if position is not None and len(position) == 4:
+            print("4 corners specified, resizing to fit.")
+            width = position[2] - position[0]
+            height = position[3] - position[1]
+            if maintainAR:
+                width, height = constrain_width_height(image_to_add, width, height)
+        else:
+            if width is not None and height is None:
+                # "Width specified, but no height."
+                if maintainAR:
+                    scale = width / image_to_add.width
+                    height = int(image_to_add.height * scale)
+                    # Maintaining AR, resizing to fit.
+                else:
+                    height = image_to_add.height
+                    # Changing width only.
+            elif height is not None and width is None:
+                # height specified, but no width.
+                if maintainAR:
+                    scale = height / image_to_add.height
+                    width = int(image_to_add.width * scale)
+                    # Maintaining AR, resizing to fit
+                else:
+                    width = image_to_add.width
+                    # Changing height only.
+
+        if width is not None and height is not None:
+            image_to_add = image_to_add.resize((width, height))
 
         im.paste(image_to_add, position)
     except Exception as e:
@@ -391,6 +419,30 @@ def element_image_base(image_to_add, element, im, margins, dimensions, **kwargs)
             print(e.message)
 
     return im
+
+def constrain_width_height(im, width, height):
+    height_scale = height / im.height
+    width_scale = width / im.width
+    if height_scale == width_scale:
+        return (width, height)
+    proposed_width = height_scale * im.width
+    proposed_height = width_scale * im.height
+
+    height_fits = proposed_height < height
+    width_fits = proposed_width < width
+
+    if height_fits and width_fits:
+        if width_scale > height_scale:
+            return (width, proposed_height)
+        else:
+            return (proposed_width, height)
+    elif width_fits:
+        return (proposed_width, height)
+    elif height_fits:
+        return (width, proposed_height)
+    else:
+        return (width, height)
+
 
 
 def get_label_context(request):
